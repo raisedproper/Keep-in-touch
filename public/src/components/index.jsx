7a25006e2fbd647fe9f816443ef5a9bfcdde7cec
colin@thelemus.com <colin@thelemus.com>
2018-10-03 19:58:20 -0700

    lit

--- public/src/components/index.jsx
+++ public/src/components/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Navbar from './Navbar/Navbar';
+import Navbar from './Navbar/index';
 
 export default class index extends Component {
     render() {
